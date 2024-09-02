d741a9b21eeb3a1e54fad02b039e2c228d5b2bf8
Eduardo da Silva Sichelero <135671476+eduardosichelero@users.noreply.github.com>
2024-09-01 23:33:00 -0300

    feat: Adiciona novos dados aos personagens

--- src/pages/CharacterDetails.js
+++ src/pages/CharacterDetails.js
@@ -1,15 +1,20 @@
-import { useRoute } from '@react-navigation/native';
+import { useRoute, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { ImageBackground } from 'react-native';
-import backgroundImage from "../images/backgroundImage.jpeg"
-
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import backgroundImage from "../images/backgroundImage.jpeg";
 
 export default function CharacterDetails() {
   const route = useRoute();
+  const navigation = useNavigation();
   const [character, setCharacter] = useState(null);
 
-  const goToShips = () => navigation.navigate("Ships");
+  // Função para navegar para a tela de naves
+// Função para navegar para a tela de naves, passando as naves como parâmetro
+const goToShips = () => navigation.navigate("Ships", { ships: character.starships });
+
+
+  // Função para navegar para a tela de filmes, passando os filmes como parâmetro
+  const goToFilms = () => navigation.navigate("Films", { films: character.films });
 
   useEffect(() => {
     setCharacter(route.params.character);
@@ -20,13 +25,21 @@ export default function CharacterDetails() {
       <View style={styles.container}>
         {character ? (
           <>
-            <Text style={styles.text}>Nome do Personagem: {character.name}</Text>
-            <Text style={styles.text}>Altura: {character.height}</Text>
+            <View>
+              <Text style={styles.text}>Nome do Personagem: {character.name}</Text>
+              <Text style={styles.text}>Altura: {character.height}</Text>
+              <Text style={styles.text}>Peso: {character.mass}</Text>
+              <Text style={styles.text}>Cor do Cabelo: {character.hair_color}</Text>
+              <Text style={styles.text}>Cor da Pele: {character.skin_color}</Text>
+              <Text style={styles.text}>Cor dos Olhos: {character.eye_color}</Text>
+              <Text style={styles.text}>Ano de Nascimento: {character.birth_year}</Text>
+            </View>
+
             <View style={styles.buttonContainer}>
               <TouchableOpacity style={styles.button} onPress={goToShips}>
                 <Text style={styles.buttonText}>Naves</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.button}>
+              <TouchableOpacity style={styles.button} onPress={goToFilms}>
                 <Text style={styles.buttonText}>Filmes</Text>
               </TouchableOpacity>
             </View>
@@ -65,7 +78,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFE81F",
   },
   buttonText: {
-    color:"black",
+    color: "black",
     fontSize: 22,
     fontWeight: "600",
   },
